cb2bc59a147451fe20156c61be86e1b22008bff1
ws <ForgedSpirit@outlook.com>
2019-03-26 21:36:46 +0800

    已<添加更新版本>

---
+++ DLL2IMPLIB/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DLL2IMPLIB.rc
+//
+#define IDD_DIALOG                      101
+#define IDC_STATIC_LOGS                 1001
+#define IDC_LIST_LOGS                   1002
+#define IDC_CHECK_BYORDINAL             1003
+#define IDC_CHECK_OPENDEF               1004
+#define IDC_CHECK_CAPITALIZE            1005
+#define IDC_HIDE_EDT_MSG                1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
